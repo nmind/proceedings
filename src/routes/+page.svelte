cfb113838f50fb430aa7845f3765b9acadcfe035
Y Sussman <ypaulsussman@gmail.com>
2023-08-07 11:01:58 -0500

    add landing-page action-cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	/>
 </div>
 
-<div class="w-full flex justify-center mt-6">
+<div class="lg:w-full flex justify-center mt-6 mx-4">
 	<div class="max-w-prose">
 		<p class="text-2xl font-light whitespace-normal break-words hyphens-auto">
 			NMIND is a collaborative dedicated to responding to the growing frustration about redundancies
@@ -19,3 +19,54 @@
 		</p>
 	</div>
 </div>
+
+<div
+	class="w-full flex justify-center items-center flex-col gap-y-6 lg:flex-row lg:gap-x-6 lg:items-stretch my-12"
+>
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<a href="/libraries">
+			<figure>
+				<img src="/images/align.png" alt="An icon with the text 'Align'" />
+			</figure>
+			<div class="card-body">
+				<p>
+					Grassroots standards for data analysis. Explore where the community is headed and share
+					your perspective!
+				</p>
+			</div>
+		</a>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<a href="/libraries">
+			<figure><img src="/images/test.png" alt="An icon with the text 'Test'" /></figure>
+			<div class="card-body">
+				<p>
+					Accessible and automated tool evaluation at your fingertips. Learn about the standards and
+					how to register your tool!
+				</p>
+			</div>
+		</a>
+	</div>
+
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<a href="/libraries">
+			<figure><img src="/images/engage.png" alt="An icon with the text 'Engage'" /></figure>
+			<div class="card-body">
+				<p>
+					NMIND relies upon the participation and contribution of community members. Find out how
+					you can join in!
+				</p>
+			</div>
+		</a>
+	</div>
+</div>
+
+<style>
+	.card:hover,
+	.card:focus {
+		transform: scale(1.05);
+		box-shadow: rgba(0, 0, 0, 0.2) 0px 25px 30px -5px, rgba(0, 0, 0, 0.15) 0px 10px 12px -6px;
+		border-color: rgb(90, 92, 106);
+	}
+</style>
