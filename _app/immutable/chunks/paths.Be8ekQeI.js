f565a9389886455ac9477e8e96f2490f0c0304e0
kaitj <kaitj@users.noreply.github.com>
2024-12-05 12:17:05 +0000

    Deploying to gh-pages from @ nmind/proceedings@a922b3c7451fd1416be92b6550ea0a88be0874be 🚀

---
+++ _app/immutable/chunks/paths.Be8ekQeI.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_13b5bg5)==null?void 0:s.base)??"/proceedings";var e;const b=((e=globalThis.__sveltekit_13b5bg5)==null?void 0:e.assets)??a;export{b as a,a as b};
